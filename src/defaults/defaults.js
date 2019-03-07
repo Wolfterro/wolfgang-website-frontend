3e3dc51291213943aaf32bb876a2d1d875343110
Wolfgang <wolfgang.pires@stormsec.com.br>
2019-03-07 20:37:58 -0300

    Atualizando defaults

--- src/defaults/defaults.js
+++ src/defaults/defaults.js
@@ -1,24 +1,60 @@
 export let websiteDefaults = {
-    "info": {
-      "id": null,
-      "owner": "Wolfgang de Almeida Pires",
-      "occupation": "Desenvolvedor Back-End",
-      "additional_info": "Com experiência em Banco de Dados, Front-End e Android.",
-      "jumbotron_image": ""
+  "info": {
+    "id": 1,
+    "owner": "Wolfgang de Almeida Pires",
+    "occupation": "Desenvolvedor Back-End",
+    "additional_info": "Com experiência em Banco de Dados, Front-End e Android.",
+    "jumbotron_image": "https://wolfgangalmeida.pythonanywhere.com/media/img/jumbotron-main.jpg"
+  },
+  "social_media": {
+    "id": 1,
+    "email": "wolfgang.almeida@yahoo.com",
+    "linkedin": "https://www.linkedin.com/in/wolfgang-almeida/",
+    "github": "https://github.com/Wolfterro",
+    "facebook": null,
+    "instagram": null,
+    "youtube": null
+  },
+  "blocks": [
+    {
+      "id": 1,
+      "title": "Quem sou eu?",
+      "content": "<p>Meu nome &eacute; Wolfgang, tenho 25 anos, moro na cidade do Rio de Janeiro e trabalho como <strong>Desenvolvedor Back-End na Storm Security &amp; Safety Systems (StormGroup).</strong></p>\r\n\r\n<p>Comecei a programar em 2015 e desde ent&atilde;o me dedico a esta profiss&atilde;o. Comecei com coisas simples como HTML, CSS e Javascript e depois parti para as linguagens de programa&ccedil;&atilde;o mais utilizadas em Back-End (Java, Python e C#).</p>\r\n\r\n<p>Estou me formando em <strong>Redes de Computadores</strong> pela <strong>Universidade Castelo Branco.</strong> Havia escolhido este curso pouco antes de seguir a carreira como desenvolvedor. Decidi continuar mesmo assim pois n&atilde;o possuia nenhum conhecimento de redes.</p>",
+      "image": "https://wolfgangalmeida.pythonanywhere.com/media/img/eu-2_Qc8eTyi.jpg",
+      "background_color": "0186c4",
+      "order": 1,
+      "button": null
     },
-    "social_media": {
-      "id": null,
-      "email": "wolfgang.almeida@yahoo.com",
-      "linkedin": "https://www.linkedin.com/in/wolfgang-almeida/",
-      "github": "https://github.com/Wolfterro",
-      "facebook": null,
-      "instagram": null,
-      "youtube": null
+    {
+      "id": 2,
+      "title": "Quais conhecimentos eu possuo?",
+      "content": "<p>Meus principais conhecimentos s&atilde;o de tecnologias e ferramentas utilizadas no desenvolvimento em solu&ccedil;&otilde;es de Back-End.</p>\r\n\r\n<p>As principais linguagens de programa&ccedil;&atilde;o que uso, s&atilde;o: <strong>Python, Java, C# (.NET), Javascript (ES6, Node.js) e um pouco de C++.</strong></p>\r\n\r\n<p>Sobre Banco de Dados, possuo experi&ecirc;ncia com os seguintes bancos SQL: <strong>MySQL, PostgreSQL, SQLite3 e um pouco de Oracle.</strong></p>\r\n\r\n<p>No quesito Front-End, possuo experi&ecirc;ncia com: <strong>HTML5, CSS3, Bootstrap 3 e 4, um pouco de Sass, Javascript, Vue.js e Django Templates.</strong></p>\r\n\r\n<p>Para uma lista completa e atualizada de quais linguagens, tecnologias, ferramentas e frameworks eu utilizo, <strong>clique no bot&atilde;o abaixo para visualizar ou baixar meu Curr&iacute;culo!</strong></p>",
+      "image": "https://wolfgangalmeida.pythonanywhere.com/media/img/Code_mAc5FOB.png",
+      "background_color": "0097dc",
+      "order": 2,
+      "button": {
+        "text": "Visualizar Currículo",
+        "type": "btn btn-secondary",
+        "link": "https://wolfgangalmeida.pythonanywhere.com/media/files/Curr%C3%ADculo_-_Wolfgang_Almeida.pdf",
+        "size": "btn-lg",
+        "left_icon": "fas fa-file-pdf",
+        "right_icon": "fas fa-download"
+      }
     },
-    "blocks": []
+    {
+      "id": 3,
+      "title": "Minhas Experiências",
+      "content": "<p>Meu trabalho como Desenvolvedor Back-End na Storm Security me proporcionou uma grande quantidade de experi&ecirc;ncias.</p>\r\n\r\n<p>Al&eacute;m dos projetos internos da empresa, como o <strong>Back-End do site da Storm</strong> e <strong>outros projetos</strong> que ainda est&atilde;o em processo de elabora&ccedil;&atilde;o e desenvolvimento, tive a chance de colaborar com projetos de grandes empresas brasileiras como a <strong>Globosat</strong> e <strong>TV Globo.</strong></p>\r\n\r\n<p>Na Globosat, trabalhei na manuten&ccedil;&atilde;o dos seguintes projetos: <strong>Globosat Play</strong>, <strong>Philos TV</strong> e <strong>Futura Play.</strong></p>\r\n\r\n<p>J&aacute; na TV Globo, pude participar do in&iacute;cio do desenvolvimento de um projeto interno, o <strong>Valora&ccedil;&atilde;o.</strong></p>\r\n\r\n<p>Para uma lista mais elaborada dos projetos que participei, consulte meu curr&iacute;culo ou meu perfil no LinkedIn.</p>",
+      "image": "https://wolfgangalmeida.pythonanywhere.com/media/img/stormgroup-clientes-2_eObfvGd.jpg",
+      "background_color": "0186c4",
+      "order": 3,
+      "button": null
+    }
+  ]
 }
 
 export let backendAddress = {
   "PROD": "https://wolfgangalmeida.pythonanywhere.com",
-  "DEV": "http://localhost:8000"
+  "DEV": "http://localhost:8000",
+  "NONE": ""
 }
